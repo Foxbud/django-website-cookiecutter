8a4d3a749f6b58b8691d0360f16ab3d1b5e7a55e
Foxbud <breadboardfox@gmail.com>
2019-05-20 13:55:01 -0400

    Added button rows.

---
+++ {{cookiecutter.project_slug}}/apps/{{cookiecutter.project_slug}}/templatetags/button_row_tags.py
@@ -0,0 +1,56 @@
+from django import template
+from django.urls import reverse
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('{{cookiecutter.project_slug}}/snippets/button_row.html')
+def button_row(
+		text,
+		color,
+		name='||',
+		script='||',
+		dest='||',
+		dest_args='||'
+):
+	# Parse arguments.
+	texts = str(text).split('|')
+	colors = str(color).split('|')
+	names = str(name).split('|')
+	scripts = str(script).split('|')
+	dests = str(dest).split('|')
+	split_dest_args = [
+		args.split(',')
+		for args
+		in str(dest_args).split('|')
+	]
+
+	# Reverse destination urls.
+	for index in range(len(dests)):
+		name = dests[index]
+		args = split_dest_args[index]
+		if name:
+			if len(args) == 1 and not args[0]:
+				dests[index] = reverse(dests[index])
+			else:
+				dests[index] = reverse(dests[index], args=args)
+
+	# Calculate number of buttons.
+	num = len(texts)
+
+	# Build context.
+	return {
+		'num_buttons': num,
+		'buttons': [
+			{
+				'text': texts[button],
+				'color': colors[button],
+				'name': names[button],
+				'script': scripts[button],
+				'dest': dests[button],
+			}
+			for button
+			in range(num)
+		],
+	}
